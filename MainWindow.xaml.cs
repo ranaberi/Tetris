6b96cc5dc48041c2fdd7e0222e03006b5fcc7d2b
Rana <ranaberi25@gmail.com>
2024-03-03 12:04:27 +0100

    UI design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,20 @@ namespace Tetris
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
